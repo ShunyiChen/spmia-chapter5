ce87a43f44f2ab3d12bb67dda362b2c931d4d420
Simeon <simeon.chen@gds.ey.com>
2022-07-27 16:24:50 +0800

    Create source code

---
+++ organization-service/src/main/java/com/thoughtmechanix/organization/Application.java
@@ -0,0 +1,17 @@
+package com.thoughtmechanix.organization;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
+
+@SpringBootApplication
+@EnableEurekaClient
+//@EnableCircuitBreaker
+@EnableHystrix
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
